a4f71a37fa429318e1834cba0235e1f97906a3c2
icemomo-hgy <i.gy@outlook.com>
2022-05-01 15:15:24 +0800

    init project

---
+++ src/main/java/com/irene/IreneApplication.java
@@ -0,0 +1,13 @@
+package com.irene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IreneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IreneApplication.class, args);
+    }
+
+}
